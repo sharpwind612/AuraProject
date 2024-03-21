47b3f977811baefa37647bbaad96b16d43713dfd
SharpWind <sharpwind612@gmail.com>
2024-03-21 21:16:02 +0800

    Add Player Controller Logic

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,3 +3,21 @@
 
 #include "Character/AuraEnemy.h"
 
+AAuraEnemy::AAuraEnemy()
+{
+
+}
+
+void AAuraEnemy::HighlightActor()
+{
+	bHighlighted = true;
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(250);
+}
+
+void AAuraEnemy::UnHighlightActor()
+{
+	bHighlighted = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	GetMesh()->SetCustomDepthStencilValue(250);
+}
