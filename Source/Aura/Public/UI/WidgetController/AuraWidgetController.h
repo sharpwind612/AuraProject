826b5990103632d0cf0ada4ffefa6896a48f22cc
SharpWind <sharpwind612@gmail.com>
2024-03-29 02:27:48 +0800

    Add Listening for Attribute Changes

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -42,6 +42,7 @@ public:
 	void SetWidgetControllerParams(const FWidgetControllerParams WCParams);
 
 	virtual void BroadcastInitialValues();
+	virtual void BindCallbacksToDependencies();
 
 protected:
 	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
