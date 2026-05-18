a8ca34a8cefac984c4838b917ae76d30544a0f29
SharpWind <sharpwind612@gmail.com>
2026-05-18 18:47:33 +0800

    Add Demon

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -206,7 +206,10 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FName("CombatSocket.LeftHand"),
 		FString("Left Hand")
 		);
-	
+	GameplayTags.CombatSocket_Tail = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("CombatSocket.Tail"),
+		FString("Tail")
+		);
 	/*
 	 * Montage Tags
 	 */
