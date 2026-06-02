e5eb3a4c37aca02abec3439ed973e5a098c5e40c
SharpWind <sharpwind612@gmail.com>
2026-06-02 17:57:21 +0800

    Showing Cooldown Time in the HUD & Add New Model

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -191,7 +191,7 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	
 	GameplayTags.Abilities_Summon = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Abilities.Summon"),
-		FString("Summon Ability Tag")
+		FString("Summon Ability Tag")                                                                                                                                                                                                                       
 		);
 	
 	GameplayTags.Abilities_Fire_FireBolt = UGameplayTagsManager::Get().AddNativeGameplayTag(
